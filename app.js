eb78a35e4573888ac26cd87f31545a47406ce2b9
Nathan Martel <nathanmartel@gmail.com>
2020-01-17 16:49:22 -0800

    Added testing and squashed a result bug

--- app.js
+++ app.js
@@ -12,36 +12,30 @@ quizButton.addEventListener('click', () => {
         let answerThree = prompt(`Finally, ${quizName}, question #3: Does the Count kinda piss people off? (Hint: The answer is yes.)`);
          
         // Test Answers
-        if (isYes(answerOne) === false) {
-            correctAnswers++;
-        }
-        if (isYes(answerTwo) === true) {
-            correctAnswers++;
-        }       
-        if (isYes(answerTwo) === true) {
-            correctAnswers++;
-        }
+        if (!isYes(answerOne)) correctAnswers++;
+        if (isYes(answerTwo)) correctAnswers++;
+        if (isYes(answerThree)) correctAnswers++;
 
         // Display Answer section
         switch (correctAnswers) {
             case 0:
                 document.getElementById('quiz-results-headline').innerText = 'So sorry!';
-                document.getElementById('quiz-results-body').innerText = `You didn't get any answers right! That's ${Math.round((correctAnswers/totalQuestions)*100)} percent!`;
+                document.getElementById('quiz-results-body').innerText = `You didn't get any answers right! That's ${Math.round((correctAnswers / totalQuestions) * 100)} percent!`;
                 document.getElementById('quiz-results-image').src = './images/count-0.png';
                 break;
             case 1:
                 document.getElementById('quiz-results-headline').innerText = 'Oops!';
-                document.getElementById('quiz-results-body').innerText = `You only got ${correctAnswers} answer right! That's just ${Math.round((correctAnswers/totalQuestions)*100)} percent!`;
+                document.getElementById('quiz-results-body').innerText = `You only got ${correctAnswers} answer right! That's just ${Math.round((correctAnswers / totalQuestions) * 100)} percent!`;
                 document.getElementById('quiz-results-image').src = './images/count-1.jpeg';
                 break;
             case 2:
                 document.getElementById('quiz-results-headline').innerText = 'Good!';
-                document.getElementById('quiz-results-body').innerText = `You got ${correctAnswers} answers right! That's ${Math.round((correctAnswers/totalQuestions)*100)} percent! Bwah ha!`;
+                document.getElementById('quiz-results-body').innerText = `You got ${correctAnswers} answers right! That's ${Math.round((correctAnswers / totalQuestions) * 100)} percent! Bwah ha!`;
                 document.getElementById('quiz-results-image').src = './images/count-2.jpeg';
                 break;
             case 3:
                 document.getElementById('quiz-results-headline').innerText = 'Excellent!';
-                document.getElementById('quiz-results-body').innerText = `You got all ${correctAnswers} answers right! That's ${Math.round((correctAnswers/totalQuestions)*100)} percent! Bwah ha haa!`;
+                document.getElementById('quiz-results-body').innerText = `You got all ${correctAnswers} answers right! That's ${Math.round((correctAnswers / totalQuestions) * 100)} percent! Bwah ha haa!`;
                 document.getElementById('quiz-results-image').src = './images/count-3.jpeg';
                 break;
             default:
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,62 @@
 // IMPORT MODULES under test here:
-// import example from '../src/example.js';
+import isYes from '../is-yes.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
-
-    //Act 
-    // Call the function you're testing and set the result to a const
-
-    //Assert
-    // Make assertions about what is expected valid result
-    assert.equal(true, false);
+test('Y is expected to return true', function(assert) {
+    const testVariable = 'Y';
+    const actualResults = isYes(testVariable);
+    const expectedResults = true;
+    assert.equal(actualResults, expectedResults);
 });
+
+test('Yes is expected to return true', function(assert) {
+    const testVariable = 'Yes';
+    const actualResults = isYes(testVariable);
+    const expectedResults = true;
+    assert.equal(actualResults, expectedResults);
+});
+
+test('YES is expected to return true', function(assert) {
+    const testVariable = 'YES';
+    const actualResults = isYes(testVariable);
+    const expectedResults = true;
+    assert.equal(actualResults, expectedResults);
+});
+
+test('yes is expected to return true', function(assert) {
+    const testVariable = 'yes';
+    const actualResults = isYes(testVariable);
+    const expectedResults = true;
+    assert.equal(actualResults, expectedResults);
+});
+
+test('no is expected to return false', function(assert) {
+    const testVariable = 'no';
+    const actualResults = isYes(testVariable);
+    const expectedResults = false;
+    assert.equal(actualResults, expectedResults);
+});
+
+test('nope is expected to return false', function(assert) {
+    const testVariable = 'nope';
+    const actualResults = isYes(testVariable);
+    const expectedResults = false;
+    assert.equal(actualResults, expectedResults);
+});
+
+test('N is expected to return false', function(assert) {
+    const testVariable = 'N';
+    const actualResults = isYes(testVariable);
+    const expectedResults = false;
+    assert.equal(actualResults, expectedResults);
+});
+
+test('asdf is expected to return false', function(assert) {
+    const testVariable = 'asdf';
+    const actualResults = isYes(testVariable);
+    const expectedResults = false;
+    assert.equal(actualResults, expectedResults);
+});
+
+
